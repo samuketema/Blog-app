46abf2dced16b571df2cbcb066bbe2138c199784
Samuel <samuelketema01@gmail.com>
2024-12-02 20:34:31 +0300

    Use the Apptheme class for the MaterialWidgets theme property.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:blog_app/core/theme/apptheme.dart';
+import 'package:blog_app/features/auth/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 
 
@@ -12,11 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:Scaffold(),
+      theme: Apptheme.darkthememode,
+      home:SigninPage(),
     );
   }
 }
