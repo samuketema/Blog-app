8f9883ed1218e3f3d1c3281727eef49ad6ab8905
Samuel <samuelketema01@gmail.com>
2024-12-05 23:18:15 +0300

    changed to signup and converted to statefulwidget.

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,10 +1,45 @@
+import 'package:blog_app/features/auth/presentation/widgets/auth_field.dart';
+import 'package:blog_app/features/auth/presentation/widgets/auth_gradient_button.dart';
 import 'package:flutter/material.dart';
- 
-class SigninPage extends StatelessWidget {
-  const SigninPage({super.key});
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
 
   @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+          Text(
+            'Sign In',
+            style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          AuthField(hintText: 'Name'),
+          SizedBox(
+            height: 15,
+          ),
+          AuthField(hintText: 'Email'),
+          SizedBox(
+            height: 15,
+          ),
+          AuthField(hintText: 'Password'),
+          SizedBox(
+            height: 20,
+          ),
+          AuthGradientButton()
+                ],
+              ),
+        ));
   }
 }
