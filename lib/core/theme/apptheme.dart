57b9e31938b364bdd698befd14f00062d156e318
Samuel <samuelketema01@gmail.com>
2024-12-06 21:58:52 +0300

    Common theme properties.

--- lib/core/theme/apptheme.dart
+++ lib/core/theme/apptheme.dart
@@ -1,7 +1,20 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class Apptheme {
+   static borderProperty({Color color = AppPallete.borderColor}) =>  OutlineInputBorder(
+        borderSide: BorderSide(
+          color:color,
+          width: 3
+        ),
+        borderRadius:  BorderRadius.circular(10)
+      );
   static final darkthememode = ThemeData.dark().copyWith(
-    scaffoldBackgroundColor: Colors.black
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      enabledBorder:borderProperty(),
+      focusedBorder: borderProperty(color:AppPallete.gradient2),
+      contentPadding: EdgeInsets.all(27)
+    )
   );
 }
