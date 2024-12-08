6484e51e068a0e7e2517fe95f6a8cb09b70b3362
Samuel <samuelketema01@gmail.com>
2024-12-08 16:36:04 +0300

    Domain layers repository for interface definition.

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,23 @@
+import 'package:blog_app/core/error/failure.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<Failure,String> >signupWithEmailPassword({ 
+    required String email,
+    required String name,
+    required String Password
+
+  }
+   
+  );
+  Future<Either<Failure,String> >loginWithEmailPassword({ 
+    required String email,
+    required String name,
+    required String Password
+
+  }
+   
+  );
+
+
+}
